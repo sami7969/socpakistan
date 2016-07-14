ce198d157ffba49eabc3d1d5b67465d177aed7d7
sami7969 <shah.samiullah@outlook.com>
2016-07-14 19:23:27 +0500

    version 1.1

--- js/main.js
+++ js/main.js
@@ -1,16 +1,43 @@
-$("#additem").on("click",function(){
-        var item = $("#val").val();
-        if(item!=''){
-          $("#container").append(" <input id='"+item+"' class='animated slideInUp gape' type='checkbox' ><label class='animated slideInUp form-element gape' for='"+item+"'>"+item+"<br></label>");
-          $("#val").val("");  
+var Data = [];
+if(d = $.parseJSON(localStorage.myData));
+if(d.length!=0){
+  for(i=0;i<d.length;i++){
+    $("#container").append(" <input id='"+d[i]+"' class='animated slideInUp gape' type='checkbox' ><label class='animated slideInUp form-element gape' for='"+d[i]+"'>"+d[i]+"<br></label>");
+    $("#"+d[i]).on("click",function(){  
+       remove(this);
+    });
 
-          $("#"+item).on("click",function(){
-             $("#"+item).remove();
-             $("label[for='"+item+"']").remove(); 
-          });  
-        }else{
-          alert("please name the todo item first");
-        }
-        
-      });
+  }  
+}
+function remove(a){
+  $(a).remove();
+  $("label[for='"+a.getAttribute('id')+"']").remove(); 
+  x=Data.indexOf(a.getAttribute('id'));
+  Data.splice(x,1);
+  save();
+  console.log(Data);
+  console.log(localStorage.myData);
+}
+function save(){
+  localStorage.myData = JSON.stringify(Data);
+}
+$("#additem").on("click",function(){
+  var item = $("#val").val();
+  Data.push(item);
+  RenderJSON(item);
+  save();
+  function RenderJSON(data){
+    if(data!=''){
+      $("#container").append("<div> <input id='"+data+"' class='animated slideInUp gape' type='checkbox' ><label class='animated slideInUp form-element gape' for='"+data+"'>"+data+"<br></label></div>");
+      $("#val").val("");  
+
+      $("#"+data).on("click",function(){
+         remove(this);
+      });  
+    }else{
+      alert("please name the todo item first");
+    }
+    
+  }    
+});
      
